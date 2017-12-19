f58a6f820392ee247c55b092d0146d30543406d9
Aditya Khandare <khandareaditya97@gmail.com>
2017-12-20 00:52:19 +0530

    initial commit

---
+++ slot.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	
+	<link rel="stylesheet" href="	css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script url:"../includes/nb.js"></script>
+
+<style>
+
+@font-face {
+		    font-family: 'harry_potterregular';
+    		src: url('fonts/harrp___-webfont.woff2') format('woff2'),
+         	url('fonts/harrp___-webfont.woff') format('woff');
+    		font-weight: normal;
+    		font-style: normal;
+
+			}
+.first{
+			    font-family: 'harry_potterregular';
+				margin-top:400px;
+				margin-left:50px;
+				font-size: 95px;
+				position:static;
+}
+
+.second{
+				font-family: 'harry_potterregular';
+				margin-top:-140px;
+				margin-left:1300px;
+				font-size: 95px;
+				position:static;
+	
+	
+	
+}
+.third{
+				font-family: 'harry_potterregular';
+				padding-top:285px;
+				padding-left:1250px;
+				font-size: 95px;
+				position:absolute;
+	
+	
+	
+}
+.fourth{
+				font-family: 'harry_potterregular';
+				padding-top:285px;
+				padding-left:50px;
+				font-size: 95px;
+				position:absolute;
+	
+	
+	
+}
+
+body {
+    			background: url("images/slots.png");
+			    background-repeat: no-repeat;
+    		size:1920px 1080px; 		
+		}
+		</style>
+	
+	
+</head>
+<body>
+
+	<div class="first"><a href="1/sign.php"><font color="#990012">GRYFFINDOR</a></div> 
+	<div class="second"><a href="2/sign.php"><font color="green">SLYTHERIN</a></div> 
+	<div class="third"><a href="3/sign.php"><font color="#000080">RAVENCLAW</a></div> 
+	<div class="fourth"><a href="4/sign.php"><font color="#FFDB58">HUFFLEPUFF</a></div> 
+	
+</body>	
+	
+</html>
+	
